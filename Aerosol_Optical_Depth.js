c203735ace2a2b53bf7fccbc4454d992a86e383e
SanjaySaifi <83391207+SanjaySaifi@users.noreply.github.com>
2021-04-29 13:01:58 +0530

    Update Aerosol_Optical_Depth.js

--- Aerosol_Optical_Depth.js
+++ Aerosol_Optical_Depth.js
@@ -1,3 +1,4 @@
+// Select the region of interest, this region can be selected by imports from the map in earth engine
 var water = ee.Geometry.Polygon(
   [[[70.1456, 24.0454],
     [70.1458, 24.0454],
@@ -6,16 +7,21 @@ var water = ee.Geometry.Polygon(
   null, false
 );
 
+// Select the satellite image 
 var collection = ee.ImageCollection("COPERNICUS/S3/OLCI")
                   .select('Oa02_radiance')
                   .filterBounds(water)
                   .filterDate('2019-10-01', '2019-12-31');
 
+// select the color composite
 var band_viz = {
   min: 0,
   max: 500,
   palette: ['black', 'cyan', 'blue', 'yellow','red']
 };
 
+// add AOD layer to map
 Map.addLayer(collection.mean(), band_viz, 'Oa02_radiance');
+
+//Set map centre and zoom level
 Map.setCenter(70.1456, 24.0454, 8);
