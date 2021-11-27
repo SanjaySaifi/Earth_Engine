34758ca7b941ce107c73eb70838b3233ffea002f
SANJAY SAIFI <sanjaysaifi241@gmail.com>
2021-11-27 10:56:29 +0530

    Update 6.Unsupervised_Clustering.js

--- Introduction_to_Google_Earth_engine/6.Unsupervised_Clustering.js
+++ Introduction_to_Google_Earth_engine/6.Unsupervised_Clustering.js
@@ -1,30 +1,40 @@
-var geometry = ee.Geometry.Polygon(
-        [[[77.43320692182444, 28.707724113129178],
-          [77.43320692182444, 28.680017221313314],
-          [77.47294653058908, 28.680017221313314],
-          [77.47294653058908, 28.707724113129178]]], null, false);
+// Define the region of interest
+var region =ee.Geometry.Polygon(
+        [[[77.42840040326975, 28.710057782656666],
+          [77.42840040326975, 28.667140056380816],
+          [77.4889968693342, 28.667140056380816],
+          [77.4889968693342, 28.710057782656666]]], null, false);
 
+//Import and filter the data
 var data=ee.ImageCollection("COPERNICUS/S2")
-              .filterDate('2019-01-01','2019-12-12');
-var input = data.mean();
+            .filterDate('2019-01-01','2019-12-31');
+ 
+// Mean the dataset
+var input=data.mean();
 
+// Clip the dataset to the region of interest
 var clip=input.clip(region);
-Map.centerObject(region, 9);
-Map.addLayer(clip);
-//Map.addLayer(clip.paint(region, 0, 2), {}, 'region');
 
-// Make the training dataset.
-var training = clip.sample({
-  region: region,
-  scale: 30,
-  numPixels: 5
+//Center the map
+Map.centerObject(region);
+
+// make a taining dataset
+var training=clip.sample({
+  region:region,
+  scale:30,
+  numPixels:5
 });
 
-// Instantiate the clusterer and train it.
-var clusterer = ee.Clusterer.wekaKMeans(15).train(training);
 
-// Cluster the input using the trained clusterer.
-var result = clip.cluster(clusterer);
+//Initiate the clusterer
+var clusterer=ee.Clusterer.wekaKMeans(15).train(training);
 
-// Display the clusters with random colors.
-Map.addLayer(result.randomVisualizer(), {}, 'clusters');
+// cluster the input
+var output=clip.cluster(clusterer);
+
+// Add clustered layer to map
+Map.addLayer(output.randomVisualizer(),{},'Unsuspervised Clusters');
+
+
+
+
