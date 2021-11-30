9a544a4016b25a00020c33b2068b231b826e9116
SANJAY SAIFI <sanjaysaifi241@gmail.com>
2021-11-30 23:36:00 +0530

    Create Basics_of_Javascript.js

---
+++ Basics_of_JavaScript/Basics_of_Javascript.js
@@ -0,0 +1,61 @@
+var hello="Hello World!";
+console.log(hello);
+print(hello);
+
+var x=5;
+var y=3;
+var z=x+y;
+print(z);
+var a,b,c,d;
+a=1,b=2,c=3,d=4;
+var sum=a+b+c+d;
+print(sum);
+
+var m="hello", n="world";
+print(m+n);
+print(m+x);
+print(m+x+y);
+print(x+y+m);
+
+// Js works from left to right for summation
+
+
+////////////////////////////////////////////////////
+// List
+var majorCities = ['Mumbai', 'Delhi', 'Chennai', 'Kolkata'];
+print(majorCities);
+
+// Dictionary
+var cityData = {
+  'city': city,
+  'population': 8400000,
+  'elevation': 930
+};
+print(cityData);
+
+/////////////////////////////////////////////////////////functions
+function funcname(){
+  print("Hello WOrld");
+}
+
+funcname();
+
+
+var greeting=function hello(name){
+  return "hello "+name+" good morning";
+}
+
+print(greeting("rohit"));
+
+
+////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
