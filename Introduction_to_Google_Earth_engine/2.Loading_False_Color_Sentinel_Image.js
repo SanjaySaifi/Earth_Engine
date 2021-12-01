64d374c81c72815aa9427d14b5d6e6799ea04d1c
SANJAY SAIFI <sanjaysaifi241@gmail.com>
2021-12-01 07:38:47 +0530

    Update 2.Loading_False_Color_Sentinel_Image.js

--- Introduction_to_Google_Earth_engine/2.Loading_False_Color_Sentinel_Image.js
+++ Introduction_to_Google_Earth_engine/2.Loading_False_Color_Sentinel_Image.js
@@ -1,16 +1,22 @@
-// Define region of interest
 var region = 
+    /* color: #98ff00 */
+    /* shown: false */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
     ee.Geometry.Polygon(
-        [[[77.9941635131836, 27.935807005891355],
-          [77.9941635131836, 27.826254354286664],
-          [78.17166137695312, 27.826254354286664],
-          [78.17166137695312, 27.935807005891355]]], null, false);
-          
+        [[[72.90982646224586, 19.09933022532271],
+          [72.90982646224586, 19.032972275564244],
+          [72.97591609237281, 19.032972275564244],
+          [72.97591609237281, 19.09933022532271]]], null, false);
+
 // Add dataset
 var data=ee.ImageCollection("COPERNICUS/S2");
 
 //Filter Dataset
-var filter=data.filterDate('2020-01-01','2020-12-31');
+var filter=data.filterDate('2020-04-01','2020-04-15');
 
 // Clip the Dataset to your region
 var clip=filter.mean().clip(region);
@@ -35,8 +41,16 @@ var viz2={
 
 
 //Center map to your region
-Map.centerObject(region);
+Map.centerObject(region,14);
 
 // Add clipped and filtered image layer to map
 Map.addLayer(clip,viz,"False Color Image");
 Map.addLayer(clip,viz2,"True Color Image");
+
+
+
+
+
+
+
+
