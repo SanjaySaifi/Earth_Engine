e4c00f4723b828e0aeeb5e39362dc1b5185a9c66
SANJAY SAIFI <sanjaysaifi241@gmail.com>
2021-12-01 07:43:40 +0530

    Update 3.CloudCover_filtering.js

--- Introduction_to_Google_Earth_engine/3.CloudCover_filtering.js
+++ Introduction_to_Google_Earth_engine/3.CloudCover_filtering.js
@@ -1,13 +1,25 @@
-//Import shapefile
-var region = ee.FeatureCollection("users/ucanwhatsappme/Ghaziabad_shapefile");
+//add region of interest
+var region = 
+    /* color: #98ff00 */
+    /* shown: false */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[72.90982646224586, 19.09933022532271],
+          [72.90982646224586, 19.032972275564244],
+          [72.97591609237281, 19.032972275564244],
+          [72.97591609237281, 19.09933022532271]]], null, false);
 
 
 // Add dataset
 var data=ee.ImageCollection("COPERNICUS/S2");
 
 //Filter Dataset
-var filter=data.filterDate('2020-01-01','2020-12-31')
-                .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',20));;
+var filter=data.filterDate('2019-03-01','2019-03-15')
+                .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',10));
 
 // Clip the Dataset to your region
 var clip=filter.mean().clip(region);
@@ -20,11 +32,18 @@ var viz={
   min:0,
   max:3000,
   bands:['B4','B3','B2'],
+  
 };
 
 
 //Center map to your region
-Map.centerObject(region);
+Map.centerObject(region,14);
 
 // Add clipped and filtered image layer to map
 Map.addLayer(clip,viz,"True Color Image");
+
+
+
+
+
+
